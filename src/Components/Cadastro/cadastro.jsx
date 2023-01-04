9da6dc0b2b519e5681a3b06a4d7233f177c8efe1
silvajpedro <belojpedro.31@gmail.com>
2023-01-03 23:38:28 -0300

    update of cadastro component

--- src/Components/Cadastro/cadastro.jsx
+++ src/Components/Cadastro/cadastro.jsx
@@ -1,39 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./cadastro_style.js";
 import logo from "../../Assets/logo.png";
 import { GlobalStyle } from "../Login/login_style.js";
 
 export default function Cadastro() {
+  const [FirstLine, setFirstLine] = useState(true);
+  const [SecondLine, setSecondLine] = useState(true);
+  const [ThirdLine, setThirdLine] = useState(true);
+  const [FourthLine, setFourthLine] = useState(true);
+  const [FifthLine, setFifthLine] = useState(true);
+  const [SixthLine, setSixthLine] = useState(true);
+  const [SeventhLine, setSeventhLine] = useState(true);
   return (
     <>
       <GlobalStyle />
       <S.RegisterMainBox>
-        <S.RegisterBox>
+        <S.RegisterBox
+          onMouseLeave={() => {
+              setFirstLine(true)
+              setSecondLine(true)
+              setThirdLine(true)
+              setFourthLine(true)
+              setFifthLine(true)
+              setSixthLine(true)
+              setSeventhLine(true)
+          }}
+        >
           <S.RegisterLogoContainer>
             <S.RegisterLogo src={logo} alt="" />
           </S.RegisterLogoContainer>
           <S.RegisterForm>
             <label>Nome*</label>
-            <S.RegisterInput type="text" />
-            <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+              setFirstLine(!FirstLine)
+              setSecondLine(true)
+              setThirdLine(true)
+              setFourthLine(true)
+              setFifthLine(true)
+              setSixthLine(true)
+              setSeventhLine(true)
+                }}
+              type="text"
+            />
+            <S.RegisterLine isOn={FirstLine}></S.RegisterLine>
             <label>Usuário*</label>
-            <S.RegisterInput type="text" />
-            <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                setFirstLine(true)
+                setSecondLine(!SecondLine)
+                setThirdLine(true)
+                setFourthLine(true)
+                setFifthLine(true)
+                setSixthLine(true)
+                setSeventhLine(true)
+                  }}
+              type="text"
+            />
+            <S.RegisterLine isOn={SecondLine}></S.RegisterLine>
             <label>Foto(url)</label>
-            <S.RegisterInput type="text" />
-            <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                setFirstLine(true)
+                setSecondLine(true)
+                setThirdLine(!ThirdLine)
+                setFourthLine(true)
+                setFifthLine(true)
+                setSixthLine(true)
+                setSeventhLine(true)
+                  }}
+              type="text"
+            />
+            <S.RegisterLine isOn={ThirdLine}></S.RegisterLine>
             <label>CPF*</label>
-            <S.RegisterInput type="text" />
-               <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                  setFirstLine(true)
+                  setSecondLine(true)
+                  setThirdLine(true)
+                  setFourthLine(!FourthLine)
+                  setFifthLine(true)
+                  setSixthLine(true)
+                  setSeventhLine(true)
+                    }}
+              type="text"
+            />
+            <S.RegisterLine isOn={FourthLine}></S.RegisterLine>
             <label>Endereço*</label>
-            <S.RegisterInput type="text" />
-               <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                setFirstLine(true)
+                setSecondLine(true)
+                setThirdLine(true)
+                setFourthLine(true)
+                setFifthLine(!FifthLine)
+                setSixthLine(true)
+                setSeventhLine(true)
+                  }}
+              type="text"
+            />
+            <S.RegisterLine isOn={FifthLine}></S.RegisterLine>
             <label>Senha*</label>
-            <S.RegisterInput type="text" />
-               <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                setFirstLine(true)
+                setSecondLine(true)
+                setThirdLine(true)
+                setFourthLine(true)
+                setFifthLine(true)
+                setSixthLine(!SixthLine)
+                setSeventhLine(true)
+              }}
+              type="text"
+            />
+            <S.RegisterLine isOn={SixthLine}></S.RegisterLine>
             <label>Confirmar Senha*</label>
-            <S.RegisterInput type="text" />
-               <S.RegisterLine></S.RegisterLine>
+            <S.RegisterInput
+              onClick={() =>{
+                setFirstLine(true)
+                setSecondLine(true)
+                setThirdLine(true)
+                setFourthLine(true)
+                setFifthLine(true)
+                setSixthLine(true)
+                setSeventhLine(!SeventhLine)
+              }}
+              type="text"
+            />
+            <S.RegisterLine isOn={SeventhLine}></S.RegisterLine>
           </S.RegisterForm>
         </S.RegisterBox>
       </S.RegisterMainBox>
--- src/Components/Cadastro/cadastro_style.js
+++ src/Components/Cadastro/cadastro_style.js
@@ -7,28 +7,8 @@ import cadeado from "../../Assets/cadeado_senha.png";
 import endereco from "../../Assets/endereço.png"
 
 export const RegisterMainBox = styled(MainBox)`
-height: 134vh;
+height: 145vh;
 background-size: cover;
-`
-export const RegisterBox = styled(LoginBox)`
-background-color: #1D0A33;
-height: 115vh;
-width:33vw ;
-`
-export const RegisterLogoContainer = styled(LogoContainer)`
-border: solid;
-height: 30vh;
-align-items: flex-end;
-`
-export const RegisterLogo = styled(Logo)`
- width: 10.55vw;
-`
-export const RegisterForm = styled(FormBox)`
-position: relative;
-top:0.5vh;
-border:solid;
-height:65vh;
-width:78%;
 input:nth-child(5){
 background-image: url(${email});
 }
@@ -44,12 +24,46 @@ input:nth-child(14){
 input:nth-child(17), input:nth-child(20){
     background-image: url(${cadeado});
 }
+div:nth-child(6), div:nth-child(9){
+    height: 0.3vh;
+}
+`
+export const RegisterBox = styled(LoginBox)`
+background-color: #1D0A33;
+height: 128vh;
+width:33vw ;
+`
+export const RegisterLogoContainer = styled(LogoContainer)`
+height: 30vh;
+align-items: flex-end;
+`
+export const RegisterLogo = styled(Logo)`
+ width: 10.55vw;
+`
+export const RegisterForm = styled(FormBox)`
+position: relative;
+top:0.5vh;
+/* border:solid; */
+height:76vh;
+width:78%;
+label{
+    top:1.4vh;
+    left: 0;
+}
 `
 export const RegisterInput = styled(UserInput)`
-/* border: solid; */
+/* border: solid red; */
 background-size:7.40%;
 height:6vh;
+background-position: 0 0.9vh;
+padding-top: 0.2vh;
+padding-left:2.4vw;
 `
 export const RegisterLine = styled(Line)`
-
+`
+export const InputBox = styled.div`
+display: flex;
+flex-direction: column;
+/* border: solid; */
+height: 10vh;
 `
