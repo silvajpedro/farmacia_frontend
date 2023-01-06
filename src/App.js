bb6af76d72a59b76edd2e85bb67cbd609e646921
silvajpedro <belojpedro.31@gmail.com>
2023-01-05 22:13:52 -0300

    test globalstyle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Rotas from "./Routes/rotas.jsx";
+import { GlobalStyle } from "./Components/Login/login_style.js";
 export default function App() {
   return (
+    <>
+    <GlobalStyle/>
     <Rotas/>
+    </>
   );
 }
 
